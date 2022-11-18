284cf8f322110df0920ec022937d0ebfe9a61550
Neo Lee <brandnewx@gmail.com>
2022-11-18 10:13:26 +0700

    Update webui.py

--- webui.py
+++ webui.py
@@ -207,9 +207,7 @@ def webui():
 if __name__ == "__main__":
     if cmd_opts.test:
         test()
-        return
-    
-    if cmd_opts.nowebui:
+    elif cmd_opts.nowebui:
         api_only()
     else:
         webui()
